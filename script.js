eec50556d6476d9732143dc6b0690a1a1fe33a26
Lalit0101 <lalitkp0101@gmail.com>
2024-07-23 20:11:27 +0530

    done

--- script.js
+++ script.js
@@ -1,4 +1,65 @@
 console.log("Hello welcome the spotify");
 
+let songIndex = 0;
+let masterPlay = document.getElementById('masterPlay');
 let audioElement = new Audio('1.mp3')
-audioElement.play();
+
+
+let songs = [
+    {
+        songName: "Salam-e-Ishq",
+        filePath: "song/1.mp3",
+        coverPath: "covers/1.path",
+    },
+    {
+        songName: "Salam-e-Ishq",
+        filePath: "song/1.mp3",
+        coverPath: "covers/1.path",
+    },
+    {
+        songName: "Salam-e-Ishq",
+        filePath: "song/1.mp3",
+        coverPath: "covers/1.path",
+    },
+    {
+        songName: "Salam-e-Ishq",
+        filePath: "song/1.mp3",
+        coverPath: "covers/1.path",
+    },
+    {
+        songName: "Salam-e-Ishq",
+        filePath: "song/1.mp3",
+        coverPath: "covers/1.path",
+    },
+    {
+        songName: "Salam-e-Ishq",
+        filePath: "song/1.mp3",
+        coverPath: "covers/1.path",
+    },
+    {
+        songName: "Salam-e-Ishq",
+        filePath: "song/1.mp3",
+        coverPath: "covers/1.path",
+    },
+    {
+        songName: "Salam-e-Ishq",
+        filePath: "song/1.mp3",
+        coverPath: "covers/1.path",
+    },
+    {
+        songName: "Salam-e-Ishq",
+        filePath: "song/1.mp3",
+        coverPath: "covers/1.path",
+    },
+    {
+        songName: "Salam-e-Ishq",
+        filePath: "song/1.mp3",
+        coverPath: "covers/1.path",
+    },
+]
+
+
+// audioElement.play();
+
+
+document.addEventListener('time')
