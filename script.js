82ae24aebd07878d2cd922e4b62a249be19cf398
Lalit0101 <lalitkp0101@gmail.com>
2024-07-23 20:26:31 +0530

    ojjk

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ console.log("Hello welcome the spotify");
 
 let songIndex = 0;
 let masterPlay = document.getElementById('masterPlay');
-let audioElement = new Audio('1.mp3')
+let audioElement = new Audio('1.mp3');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
 
 
 let songs = [
@@ -60,6 +62,21 @@ let songs = [
 
 
 // audioElement.play();
+masterPlay.addEventListener('click', () =>{
+    if(audioElement.paused || audioElement.currentTime <= 0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-circle-pause');
+        masterPlay.classList.add('fa-circle-play');
+        gif.style.opacity = 0;
+    }
+})
 
-
-document.addEventListener('time')
+myProgressBar.addEventListener('timeupdate', ()=>{
+    console.log('timeupdate');
+})
