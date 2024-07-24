89d635813911731be1fbd69432eae8271709eff8
Lalit0101 <lalitkp0101@gmail.com>
2024-07-24 19:59:31 +0530

    finally

--- script.js
+++ script.js
@@ -1,79 +1,79 @@
-// console.log("Hello welcome the spotify");
+console.log("Hello welcome the spotify");
 
-// let songIndex = 0;
-// let masterPlay = document.getElementById("masterPlay");
-// let audioElement = new Audio("1.mp3");
-// let myProgressBar = document.getElementById("myProgressBar");
-// let gif = document.getElementById("gif");
+let songIndex = 0;
+let masterPlay = document.getElementById("masterPlay");
+let audioElement = new Audio("1.mp3");
+let myProgressBar = document.getElementById("myProgressBar");
+let gif = document.getElementById("gif");
 
-// let songs = [
-//   {
-//     songName: "Salam-e-Ishq",
-//     filePath: "song/1.mp3",
-//     coverPath: "covers/1.path",
-//   },
-//   {
-//     songName: "Salam-e-Ishq",
-//     filePath: "song/1.mp3",
-//     coverPath: "covers/1.path",
-//   },
-//   {
-//     songName: "Salam-e-Ishq",
-//     filePath: "song/1.mp3",
-//     coverPath: "covers/1.path",
-//   },
-//   {
-//     songName: "Salam-e-Ishq",
-//     filePath: "song/1.mp3",
-//     coverPath: "covers/1.path",
-//   },
-//   {
-//     songName: "Salam-e-Ishq",
-//     filePath: "song/1.mp3",
-//     coverPath: "covers/1.path",
-//   },
-//   {
-//     songName: "Salam-e-Ishq",
-//     filePath: "song/1.mp3",
-//     coverPath: "covers/1.path",
-//   },
-//   {
-//     songName: "Salam-e-Ishq",
-//     filePath: "song/1.mp3",
-//     coverPath: "covers/1.path",
-//   },
-//   {
-//     songName: "Salam-e-Ishq",
-//     filePath: "song/1.mp3",
-//     coverPath: "covers/1.path",
-//   },
-//   {
-//     songName: "Salam-e-Ishq",
-//     filePath: "song/1.mp3",
-//     coverPath: "covers/1.path",
-//   },
-//   {
-//     songName: "Salam-e-Ishq",
-//     filePath: "song/1.mp3",
-//     coverPath: "covers/1.path",
-//   },
-// ];
+let songs = [
+  {
+    songName: "Salam-e-Ishq",
+    filePath: "song/1.mp3",
+    coverPath: "covers/1.path",
+  },
+  {
+    songName: "Salam-e-Ishq",
+    filePath: "song/1.mp3",
+    coverPath: "covers/1.path",
+  },
+  {
+    songName: "Salam-e-Ishq",
+    filePath: "song/1.mp3",
+    coverPath: "covers/1.path",
+  },
+  {
+    songName: "Salam-e-Ishq",
+    filePath: "song/1.mp3",
+    coverPath: "covers/1.path",
+  },
+  {
+    songName: "Salam-e-Ishq",
+    filePath: "song/1.mp3",
+    coverPath: "covers/1.path",
+  },
+  {
+    songName: "Salam-e-Ishq",
+    filePath: "song/1.mp3",
+    coverPath: "covers/1.path",
+  },
+  {
+    songName: "Salam-e-Ishq",
+    filePath: "song/1.mp3",
+    coverPath: "covers/1.path",
+  },
+  {
+    songName: "Salam-e-Ishq",
+    filePath: "song/1.mp3",
+    coverPath: "covers/1.path",
+  },
+  {
+    songName: "Salam-e-Ishq",
+    filePath: "song/1.mp3",
+    coverPath: "covers/1.path",
+  },
+  {
+    songName: "Salam-e-Ishq",
+    filePath: "song/1.mp3",
+    coverPath: "covers/1.path",
+  },
+];
 
-// // audioElement.play();
-// masterPlay.addEventListener("click", () => {
-//   if (audioElement.paused || audioElement.currentTime <= 0) {
-//     audioElement.play();
-//     masterPlay.classList.remove("fa-circle-play");
-//     masterPlay.classList.add("fa-circle-pause");
-//     gif.style.opacity = 1;
-//   } else {
-//     audioElement.pause();
-//     masterPlay.classList.remove("fa-circle-pause");
-//     masterPlay.classList.add("fa-circle-play");
-//     gif.style.opacity = 0;
-//   }
-// });
+// audioElement.play();
+masterPlay.addEventListener("click", () => {
+  if (audioElement.paused || audioElement.currentTime <= 0) {
+    audioElement.play();
+    masterPlay.classList.remove("fa-circle-play");
+    masterPlay.classList.add("fa-circle-pause");
+    gif.style.opacity = 1;
+  } else {
+    audioElement.pause();
+    masterPlay.classList.remove("fa-circle-pause");
+    masterPlay.classList.add("fa-circle-play");
+    gif.style.opacity = 0;
+  }
+});
 
-// audioElement.addEventListener("timeupdate", () => {
-//   console.log("timeupdate");
-// });
+audioElement.addEventListener("timeupdate", () => {
+  console.log("timeupdate");
+});
