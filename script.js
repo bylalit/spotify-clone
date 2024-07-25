3e9406c06f0215dc45570294fa9043edad834313
Lalit0101 <lalitkp0101@gmail.com>
2024-07-25 09:23:12 +0530

    okk

--- script.js
+++ script.js
@@ -2,65 +2,71 @@ console.log("Hello welcome the spotify");
 
 let songIndex = 0;
 let masterPlay = document.getElementById("masterPlay");
-let audioElement = new Audio("1.mp3");
+let audioElement = new Audio("clipe/songs/1.mp3");
 let myProgressBar = document.getElementById("myProgressBar");
 let gif = document.getElementById("gif");
+
+let songItem = Array.from(document.getElementsByClassName("songItem"));
 
 let songs = [
   {
     songName: "Salam-e-Ishq",
-    filePath: "song/1.mp3",
-    coverPath: "clipe/covers/1.path",
+    filePath: "clipe/songs/1.mp3",
+    coverPath: "clipe/covers/1.jpg",
   },
   {
     songName: "Salam-e-Ishq 2",
-    filePath: "clipe/song/2.mp3",
-    coverPath: "clipe/covers/2.path",
+    filePath: "clipe/songs/2.mp3",
+    coverPath: "clipe/covers/2.jpg",
   },
   {
     songName: "Salam-e-Ishq 3",
-    filePath: "clipe/song/3.mp3",
-    coverPath: "clipe/covers/3.path",
+    filePath: "clipe/songs/3.mp3",
+    coverPath: "clipe/covers/3.jpg",
   },
   {
     songName: "Salam-e-Ishq 4",
-    filePath: "clipe/song/4.mp3",
-    coverPath: "covers/4.path",
+    filePath: "clipe/songs/4.mp3",
+    coverPath: "clipe/covers/4.jpg",
   },
   {
     songName: "Salam-e-Ishq 5",
-    filePath: "clipe/song/5.mp3",
-    coverPath: "clipe/covers/5.path",
+    filePath: "clipe/songs/5.mp3",
+    coverPath: "clipe/covers/5.jpg",
   },
   {
     songName: "Salam-e-Ishq 6",
-    filePath: "clipe/song/6.mp3",
-    coverPath: "clipe/covers/6.path",
+    filePath: "clipe/songs/6.mp3",
+    coverPath: "clipe/covers/6.jpg",
   },
   {
     songName: "Salam-e-Ishq 7",
-    filePath: "clipe/song/7.mp3",
-    coverPath: "clipe/covers/7.path",
+    filePath: "clipe/songs/7.mp3",
+    coverPath: "clipe/covers/7.jpg",
   },
   {
     songName: "Salam-e-Ishq 8",
-    filePath: "clipe/song/8.mp3",
-    coverPath: "clipe/covers/8.path",
+    filePath: "clipe/songs/8.mp3",
+    coverPath: "clipe/covers/8.jpg",
   },
   {
     songName: "Salam-e-Ishq 9",
-    filePath: "clipe/song/9.mp3",
-    coverPath: "clipe/covers/9.path",
+    filePath: "clipe/songs/9.mp3",
+    coverPath: "clipe/covers/9.jpg",
   },
   {
-    songName: "Salam-e-Ishq 0",
-    filePath: "clipe/song/10.mp3",
-    coverPath: "clipe/covers/10.path",
+    songName: "Salam-e-Ishq 10",
+    filePath: "clipe/songs/10.mp3",
+    coverPath: "clipe/covers/10.jpg",
   },
 ];
 
 
-songs.forEach
+songItem.forEach((element, i) =>{
+  // console.log(element,i);
+  element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+  element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+})
 
 // audioElement.play();
 masterPlay.addEventListener("click", () => {
@@ -87,3 +93,20 @@ audioElement.addEventListener("timeupdate", () => {
 myProgressBar.addEventListener("change", ()=>{
   audioElement.currentTime = myProgressBar.value * audioElement.duration / 100;
 })
+
+
+const masteAllPlays = ()=>{
+  Array.from(document.getElementsByClassName("songItemPlay")).forEach((element)=>{
+    element.classList.remove("fa-circle-pause");
+    element.classList.add("fa-circle-play");
+  })
+}
+
+Array.from(document.getElementsByClassName("songItemPlay")).forEach((element)=>{
+  element.addEventListener("click", (e)=>{
+    // console.log(e.target);
+    masteAllPlays();
+    e.target.classList.remove("fa-circle-play");
+    e.target.classList.add("fa-circle-pause");
+  })
+});
